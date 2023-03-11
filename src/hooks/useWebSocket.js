d826b4f6e036feb70f5442f00da06461801c1934
hulihushao <2037499749@qq.com>
2023-03-11 11:33:15 +0000

    Update useWebSocket.js

--- src/hooks/useWebSocket.js
+++ src/hooks/useWebSocket.js
@@ -1,4 +1,5 @@
 import {ref} from "vue"
+import { ElMessage} from 'element-plus'
 export function useWebSocket(refValue,user) {
     let socket=ref(null)
   socket.value = new WebSocket("ws://192.168.241.2:8081");
@@ -6,19 +7,19 @@ export function useWebSocket(refValue,user) {
     alert( "连接服务器成功")
     useSend(socket,user)
   });
-  useOnMessage(socket,refValue)
+  socket.value.addEventListener("message", function (e) {
+    refValue.value.push(e.data)
+  });
   socket.value.onerror = (error) => {
     
   };
   socket.value.onclose = () => {
-
+    ElMessage.error("连接已关闭")
   };
-  return socket
+  
 }
 export function useOnMessage(socket,values){
-    socket.addEventListener("message", function (e) {
-    values.value.push(e.data)
-  });
+    
 }
 export function useSend(socket,message){
   socket.value.send(JSON.stringify(message))
