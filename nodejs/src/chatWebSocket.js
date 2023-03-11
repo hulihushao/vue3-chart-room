c6c1f7b4c29ec89e406710fb1df89c9be9b52431
hulihushao <2037499749@qq.com>
2023-03-11 13:26:47 +0000

    Update chatWebSocket.js

--- nodejs/src/chatWebSocket.js
+++ nodejs/src/chatWebSocket.js
@@ -11,23 +11,43 @@ function chatWebSocketServer() {
     console.log("disconnected");
   });
 
+  let broadcast = (message) => {
+    server.clients.forEach(function each(client) {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(message);
+      }
+    });
+  };
   server.on("connection", function connection(ws, req) {
-
     const ip = req.socket.remoteAddress;
     const port = req.socket.remotePort;
     const clientName = ip + ":" + port;
 
     console.log("%s is connected ", clientName);
 
-//    ws.send("Welcome " + clientName);
-
     ws.on("message", function incoming(message) {
       console.log("received: %s from %s", message, clientName);
-      server.clients.forEach(function each(client) {
-        if (client.readyState === WebSocket.OPEN) {
-         // client.send(clientName + " -> " + message);
-        }
-      });
+      const obj = JSON.parse(message);
+      switch (obj.type) {
+        case 1:
+          broadcast({
+            type: 1,
+            nickname: obj.nickname,
+            uid: obj.uid,
+            msg: `${obj.nickname}进入了聊天室`,
+            date: obj.date,
+          });
+          break;
+        case 2:
+          broadcast({
+            type: 2,
+            nickname: obj.nickname,
+            uid: obj.uid,
+            msg: obj.msg,
+            date: obj.date,
+          });
+          break;
+      }
     });
   });
 }
