86a262ac20f3763205702d159b57c8bc86bd6b8a
hulihushao <2037499749@qq.com>
2023-05-29 16:53:23 +0800

    sx

--- vite.config.js
+++ vite.config.js
@@ -22,7 +22,7 @@ export default defineConfig({
     },
   },
   build: {
-    outDir: "dist", // 指定输出路径
+    //outDir: "dist", // 指定输出路径
     cssCodeSplit: true, // 启用 CSS 代码拆分
     sourcemap: false, // 构建后是否生成 source map 文件
     chunkSizeWarningLimit: 1500,
